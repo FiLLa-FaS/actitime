ca823fe6854b826e0e13893f81aac76fe3ff5221
FiLLa-FaS <fillafas@yandex.ru>
2020-05-20 20:39:15 +0300

    Updates

---
+++ precache-manifest.5e0e2b54a6c93f88eae221902f897aea.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb174dbb3725bc925a8bc15de02ca66c",
+    "url": "/actitime/index.html"
+  },
+  {
+    "revision": "7b8d86a82a3aae2a524c",
+    "url": "/actitime/static/css/2.76cf7f4f.chunk.css"
+  },
+  {
+    "revision": "80e5be7312106eebe69d",
+    "url": "/actitime/static/css/main.0a10cf6d.chunk.css"
+  },
+  {
+    "revision": "7b8d86a82a3aae2a524c",
+    "url": "/actitime/static/js/2.85c0dfac.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/actitime/static/js/2.85c0dfac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80e5be7312106eebe69d",
+    "url": "/actitime/static/js/main.37ee8769.chunk.js"
+  },
+  {
+    "revision": "9d0285dd25eeab9aa0c6",
+    "url": "/actitime/static/js/runtime-main.71609293.js"
+  },
+  {
+    "revision": "fba9ba167ec6e4f7f92516862b88d567",
+    "url": "/actitime/static/media/icn_arrow_down.fba9ba16.svg"
+  },
+  {
+    "revision": "c3cfed1b9e8845e51df8c7d57e79dd68",
+    "url": "/actitime/static/media/icn_arrow_link.c3cfed1b.svg"
+  },
+  {
+    "revision": "d361c9b99988656af9612d6a39143331",
+    "url": "/actitime/static/media/icn_arrow_right.d361c9b9.svg"
+  },
+  {
+    "revision": "d86695adbcf07fe1b03290d85a4de73a",
+    "url": "/actitime/static/media/icn_burger.d86695ad.svg"
+  },
+  {
+    "revision": "e76f12b2845616e4ed4f7e533d634ec3",
+    "url": "/actitime/static/media/icn_close.e76f12b2.svg"
+  },
+  {
+    "revision": "efd69f38958b00ee916348879798d42a",
+    "url": "/actitime/static/media/icn_logo.efd69f38.svg"
+  },
+  {
+    "revision": "1a746d07975009fa9adafce3704df17d",
+    "url": "/actitime/static/media/icn_logo_white.1a746d07.svg"
+  }
+]);
